8149ea0b82c7e4d4db4f43f55da02ed161b9cc82
JAGADEESHHUB <jagadeeshkoffl@gmail.com>
2025-06-26 15:29:47 +0530

    Commit without // comments

---
+++ src/main/java/com/JPA/Hibernate/repositories/AuthorRepo.java.bak
@@ -0,0 +1,7 @@
+package com.JPA.Hibernate.repositories;
+
+import com.JPA.Hibernate.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepo extends JpaRepository<Author, Integer> {
+}
---
+++ src/main/java/com/JPA/Hibernate/service/AuthorService.java
@@ -0,0 +1,22 @@
+package com.JPA.Hibernate.service;
+
+
+import com.JPA.Hibernate.model.Author;
+import com.JPA.Hibernate.repositories.AuthorRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+    private AuthorRepo authorRepo;
+
+    public AuthorService(AuthorRepo authorRepo){
+        this.authorRepo=authorRepo;
+    }
+
+    public String SaveAuthor(Author author){
+        authorRepo.save(author);
+        return "Saved successfully";
+    }
+
+}
